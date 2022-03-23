e552303fd571778179cc350411883e8361a5c8db
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-03-22 17:31:50 -0700

    Cars Router boilerplate is complete

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,19 @@
-// DO YOUR MAGIC
+const express = require('express')
+const Cars = require('./cars-model')
+const router = require('express').Router()
+
+
+router.getAll('/', (req,res,next) => {
+
+})
+
+router.getById('/:id', (req,res,next) => {
+    
+})
+
+router.create('/:id', (req,res,next) => {
+    
+})
+
+module.exports = router;
+
